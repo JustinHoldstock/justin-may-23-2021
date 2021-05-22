e80ba1b08f2920c7762abbca0e4dd0988d914c80
JustinHoldstock <justinholdstock91@gmail.com>
2021-05-22 12:07:56 -0600

    feat: totals

--- src/app/components/order-table/order-table.component.ts
+++ src/app/components/order-table/order-table.component.ts
@@ -1,15 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Order } from 'src/app/state/order.model';
+
+interface OrderDisplay extends Order {
+  total: number;
+}
 
 @Component({
   selector: 'app-order-table',
   templateUrl: './order-table.component.html',
   styleUrls: ['./order-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OrderTableComponent implements OnInit {
-  @Input() orders: Order[];
+  @Input() orders$: Observable<Order[]>;
+
+  displayOrders$: Observable<OrderDisplay[]>;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.displayOrders$ = this.orders$.pipe(
+      map((orders) => {
+        let total = 0;
+        return orders.map((order) => {
+          total += order.size;
+          return {
+            ...order,
+            total,
+          };
+        });
+      })
+    );
+  }
 }
