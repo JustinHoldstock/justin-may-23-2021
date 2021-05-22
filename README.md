8b8f4f07b575467f4a550a2b5379747cf884766b
JustinHoldstock <justinholdstock91@gmail.com>
2021-05-22 15:39:34 -0600

    chore: styling and theming

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.0.
 
+To get started, run `npm i` or `npm ci`;
+If you don't have the Angular dev tools installed, get the tools to run this project `npm install -g @angular/cli`;
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { OrderTableComponent } from './components/order-table/order-table.component';
 import { GroupControlComponent } from './components/group-control/group-control.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OrderTableComponent,
-    GroupControlComponent
-  ],
+  declarations: [AppComponent, OrderTableComponent, GroupControlComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
