9528889ca59163556bcfdda479c010bc3e7c8792
JustinHoldstock <justinholdstock91@gmail.com>
2021-05-22 19:09:59 -0600

    test: add tests for order service

--- TODO.md
+++ TODO.md
@@ -4,6 +4,7 @@ Notes:
 - Questions.MD for technical Qs
 - pause stream on tab navigation
 - only update total if previous has changed, better aggregate calculation 
+- I really want to add Jest to this project, but had to opt for built in Jasmine due to time
 
 Stretch goals
 - total bar underneath each row
--- src/app/services/order.service.spec.ts
+++ src/app/services/order.service.spec.ts
@@ -1,16 +1,101 @@
-import { TestBed } from '@angular/core/testing';
-
+import { OrderDataService } from './order-data.service';
 import { OrderService } from './order.service';
 
-describe('OrderService', () => {
+fdescribe('OrderService', () => {
   let service: OrderService;
 
+  OrderDataService.prototype.connect = jasmine.createSpy();
+  OrderDataService.prototype.close = jasmine.createSpy();
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrderService);
+    service = new OrderService();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('constructor()', () => {
+    it('should kickstart the websocket connection', () => {
+      expect((service as any).orderDataService.connect).toHaveBeenCalled();
+    });
+
+    it('should subscribe to the message obsevable to update orders', () => {
+      service.updateOrders = jasmine.createSpy();
+      (service as any).orderDataService.messages$$.next({ asks: [] });
+      (service as any).orderDataService.messages$$.next({ bids: [] });
+      expect(service.updateOrders).toHaveBeenCalledTimes(2);
+    });
+
+    it('should setup a query for asks', (done) => {
+      service.asks$.subscribe((asks) => {
+        // This occurring is all we need to confim the query is setup
+        expect(asks).toBeDefined();
+        done();
+      });
+    });
+
+    it('should setup a query for bids', (done) => {
+      service.bids$.subscribe((bids) => {
+        // This occurring is all we need to confim the query is setup
+        expect(bids).toBeDefined();
+        done();
+      });
+    });
+  });
+
+  describe('updateOrders()', () => {
+    it('should update the ask store', (done) => {
+      service.updateOrders([[123, 321]]);
+
+      service.asks$.subscribe((asks) => {
+        expect(asks.length).toEqual(1);
+        done();
+      });
+    });
+
+    it('should update the bid store store', (done) => {
+      service.updateOrders([], [[123, 321]]);
+
+      service.bids$.subscribe((bids) => {
+        expect(bids.length).toEqual(1);
+        done();
+      });
+    });
+  });
+
+  describe('updateStore()', () => {
+    it('should remove an entity from the store if the order is empty', () => {
+      const removeStub = jasmine.createSpy();
+      (service as any).askStore.remove = removeStub;
+
+      (service as any).updateStore([123, 0], (service as any).askStore);
+
+      expect(removeStub).toHaveBeenCalled();
+    });
+
+    it('should upsert the order into the store', () => {
+      const upStub = jasmine.createSpy();
+      (service as any).askStore.upsert = upStub;
+
+      (service as any).updateStore([123, 321], (service as any).askStore);
+
+      expect(upStub).toHaveBeenCalled();
+    });
+  });
+
+  describe('cleanup', () => {
+    it('should close the websocket connection', () => {
+      service.ngOnDestroy();
+      expect((service as any).orderDataService.close).toHaveBeenCalled();
+    });
+
+    it('should cleanup message subscription', () => {
+      service.ngOnDestroy();
+      service.updateOrders = jasmine.createSpy();
+      (service as any).orderDataService.messages$$.next({ asks: [] });
+      (service as any).orderDataService.messages$$.next({ bids: [] });
+      expect(service.updateOrders).not.toHaveBeenCalled();
+    });
   });
 });
